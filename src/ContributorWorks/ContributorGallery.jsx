80b0c7a4a5d6524007b56afef6a4b797e330629c
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-10-05 21:48:43 -0500

    Update ContributorGallery.jsx

--- src/ContributorWorks/ContributorGallery.jsx
+++ src/ContributorWorks/ContributorGallery.jsx
@@ -10,19 +10,19 @@ export default function ContributorGallery (props){
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_apiKey,
-    authDomain: import.meta.env.VITE_FIREBASE_authDomain,
-    projectId: import.meta.env.VITE_FIREBASE_projectId,
-    storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
-    appId: import.meta.env.VITE_FIREBASE_appId,
-    measurementId: import.meta.env.VITE_FIREBASE_measurementId
-  };
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+//     authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+//     projectId: import.meta.env.VITE_FIREBASE_projectId,
+//     storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+//     messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+//     appId: import.meta.env.VITE_FIREBASE_appId,
+//     measurementId: import.meta.env.VITE_FIREBASE_measurementId
+//   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
 
     
     return(<>
