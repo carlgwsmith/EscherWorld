7a950bca4b467e4c88ed2d23b80c475196c46b9e
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-11-20 22:09:28 -0600

    Update supabaseClient.jsx

--- src/Config/supabaseClient.jsx
+++ src/Config/supabaseClient.jsx
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseKey = import.meta.env.VITE_SUPABASE_API;
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_API;
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
